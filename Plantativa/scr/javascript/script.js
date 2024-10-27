16b3691fcf8a4e4e804c43e7d57c3a39d3f32620
Foxxex <98045648+Deejetos@users.noreply.github.com>
2024-10-27 18:55:47 -0300

    Planta

--- Plantativa/scr/javascript/script.js
+++ Plantativa/scr/javascript/script.js
@@ -1,6 +1,38 @@
+import i from './index.html';
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
+    })
+
+    const sections = $('section')
+    const navItens = $('nav-item')
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        
+        let activeSectionIndex = 0;
+
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        }
+        else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.5)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionbottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) { 
+                activeSectionIndex = i;
+                return false;
+            }
+            
+        })
+
+        $(navItem[activeSectionIndex]).addClass('active');
     })
 })
